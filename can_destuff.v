ccef8b748f7c3268efd77dd33acf2c15ac610b85
Carol Medeiros <cpm@cin.ufpe.br>
2017-05-26 18:25:19 -0300

    criacao do destuff
    criacao do destuff  (sem teste)

--- can_destuff.v
+++ can_destuff.v
@@ -4,35 +4,60 @@ module can_destuff
 		input i_Ds_Serial,
 		input i_cont_0,
 		input i_cont_1,
-		output o_Ds_Serial
+		input i_bit_index,
+		output o_bit_index
+		//output o_flag_destuff
 	);
 	
-	//caso seja o quinto bit repetido chama o proprio metodo atual em rx se nao salva o valor normalmente em rx
-	/*
-	if (i_Ds_Serial == 0)
-			i_cont_0 = i_cont_0 + 1;
-		else
-			i_cont_1 = i_cont_1 + 1;
-		if (i_cont_0 < 6 && i_cont_1 < 6)
-			begin
-				o_Ds_Serial <= i_Ds_Serial;
-				#(c_BIT_PERIOD);
-			end
-		else
-			if (i_cont_0 == 6)
-				begin
-					i_cont_0 <= 1; //contador zera dps soma 1 do novo bit
-					r_Rx_Serial <= 1; //bit stuffing
-					r_Rx_Serial <= i_start; //sexto bit 0
-					#(c_BIT_PERIOD);
-				end
-			else
-				begin
-					i_cont_1 <= 1; //contador zera dps soma 1 do novo bit
-					r_Rx_Serial <= 0; //bit stuffing
-					r_Rx_Serial <= i_start; //sexto bit 1
-					#(c_BIT_PERIOD);
-				end
-	*/			
+	reg Ds_Serial = 1'b0;
+	reg cont_0 = 1'b0;
+	reg cont_1 = 1'b0;
+	reg bit_prox_index = 1'b0;
+	//reg flag_destuff = 1'b0;
+	
+	always @(posedge i_Clock)
+		begin
+			Ds_Serial <= i_Ds_Serial;
+			cont_0 <= i_cont_0;
+			cont_1 <= i_cont_1;
+			bit_prox_index <= i_bit_index;
+			//flag_destuff <= o_flag_destuff;
+		end
 
+	always @(posedge i_Clock)
+		begin
+			// O destuff só pode considerar que ouve bits iguais sequenciais caso um dos contadores seja 0 e o outro 5.
+			// Se esse cuidado não for tomado os contadores ficarão sendo somados individualmente
+			// até que o primeiro chegue a 5 sem necessariamente ser sequencialmente.
+			// Assim sendo o código abaixo fica fazendo um switch entre o cont_0 e o cont_1
+			// onde um só será somado se o outro estiver zerado.
+			// Caso não esteja zerado o código passa a somar o contador do outro bit e zera o contador anterior.
+			if (Ds_Serial == 0) 0000010
+				if(cont_1 == 0)
+					cont_0 <= cont_0 + 1;
+				else
+					begin
+						cont_1 <= 0
+						cont_0 <= cont_0 + 1;
+					end
+			else
+				if (cont_0 == 0)
+					cont_1 <= cont_1 + 1;
+				else
+					begin
+						cont_0 <= 0;
+						cont_1 <= cont_1 + 1;
+					end
+			
+			if (cont_0 == 5 || cont_1 == 5)
+				begin
+					//flag_destuff <= 1; // se for o quinto bit aciona a flag pra o can_rx ignorar o proximo bit lá
+					bit_prox_index <= bit_prox_index + 1; // Se for o quinto bit o destuff pula uma posição do índice (pula o bit stuff)
+				end									 // O can_rx deve salvar o bit atual para só então atualizar a posição atual
+														 // do bit com o valor de bit_index.
+		end
+	
+	//assign o_flag_destuff = flag_destuff;
+	assign o_bit_index = bit_prox_index;
+	
 endmodule
