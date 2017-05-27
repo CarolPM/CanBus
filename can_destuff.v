b2e6583fbc08c34b013e683899cd25ead19627fc
Carol Medeiros <cpm@cin.ufpe.br>
2017-05-27 04:30:47 -0300

    destuff_fixing
    destuff com erro

--- can_destuff.v
+++ can_destuff.v
@@ -4,26 +4,28 @@ module can_destuff
 		input i_Ds_Serial,
 		input i_cont_0,
 		input i_cont_1,
-		input i_bit_index,
-		output o_bit_index
-		//output o_flag_destuff
+		//input i_bit_index,
+		//output o_bit_index,
+		output o_cont_0,
+		output o_cont_1,
+		output o_flag_destuff
 	);
 	
-	parameter CLKS_PER_BIT  			= 10;
+	parameter CLKS_PER_BIT  = 10;
 	
 	reg Ds_Serial = 1'b0;
-	reg cont_0 = 1'b0;
-	reg cont_1 = 1'b0;
-	reg bit_prox_index = 1'b0;
-	//reg flag_destuff = 1'b0;
+	integer cont_0 = 0;
+	integer cont_1 = 0;
+	//reg bit_prox_index = 1'b0;
+	reg flag_destuff = 1'b0;
 	
 	always @(posedge i_Clock)
 		begin
 			Ds_Serial <= i_Ds_Serial;
 			cont_0 <= i_cont_0;
 			cont_1 <= i_cont_1;
-			bit_prox_index <= i_bit_index;
-			//flag_destuff <= o_flag_destuff;
+			//bit_prox_index <= i_bit_index;
+			flag_destuff <= o_flag_destuff;
 		end
 
 	always @(posedge i_Clock)
@@ -34,32 +36,39 @@ module can_destuff
 			// Assim sendo o código abaixo fica fazendo um switch entre o cont_0 e o cont_1
 			// onde um só será somado se o outro estiver zerado.
 			// Caso não esteja zerado o código passa a somar o contador do outro bit e zera o contador anterior.
+			$display("Destuffing");
 			if (Ds_Serial == 0)
-				if(cont_1 == 0)
-					cont_0 <= cont_0 + 1;
-				else
-					begin
-						cont_1 <= 0;
-						cont_0 <= cont_0 + 1;
-					end
+				begin
+					if(cont_1 == 0)
+						cont_0 = cont_0 + 1;
+					else
+						begin
+							cont_1 = 0;
+							cont_0 = cont_0 + 1;
+						end
+				end
 			else
-				if (cont_0 == 0)
-					cont_1 <= cont_1 + 1;
-				else
-					begin
-						cont_0 <= 0;
-						cont_1 <= cont_1 + 1;
-					end
-			
+				begin
+					if (cont_0 == 0)
+						cont_1 = cont_1 + 1;
+					else
+						begin
+							cont_0 = 0;
+							cont_1 = cont_1 + 1;
+						end
+				end
+				
 			if (cont_0 == 5 || cont_1 == 5)
 				begin
-					//flag_destuff <= 1; // se for o quinto bit aciona a flag pra o can_rx ignorar o proximo bit lá
-					bit_prox_index <= bit_prox_index + 1; // Se for o quinto bit o destuff pula uma posição do índice (pula o bit stuff)
+					flag_destuff <= 1; // se for o quinto bit aciona a flag pra o can_rx ignorar o proximo bit lá
+					//bit_prox_index <= bit_prox_index + 1; // Se for o quinto bit o destuff pula uma posição do índice (pula o bit stuff)
 				end									 // O can_rx deve salvar o bit atual para só então atualizar a posição atual
 														 // do bit com o valor de bit_index.
 		end
 	
-	//assign o_flag_destuff = flag_destuff;
-	assign o_bit_index = bit_prox_index;
+	assign o_flag_destuff = flag_destuff;
+	//assign o_bit_index = bit_prox_index;
+	assign o_cont_0 = cont_0;
+	assign o_cont_1 = cont_1;
 	
 endmodule
--- can_tb.v
+++ can_tb.v
@@ -19,7 +19,9 @@ module can_tb ();
   parameter c_CLOCK_PERIOD_NS = 100;
   parameter c_CLKS_PER_BIT    = 10;
   parameter c_BIT_PERIOD      = 1000;
-   
+  parameter s_DESTUFF  		   = 0;
+  parameter s_RX		   		= 1;
+  
   reg r_Clock = 0;
   reg r_Tx_DV = 0;
   wire w_Tx_Done;
@@ -27,9 +29,11 @@ module can_tb ();
   reg r_Rx_Serial = 1;
   reg r_Ds_Serial = 1;
   wire [0:107] w_Rx_Byte; //start+identifier+RTR+IDE+RESERVED0+data
-  reg cont_0 = 1'b0;
-  reg cont_1 = 1'b0;
-   
+  wire cont_0 = 0;
+  wire cont_1 = 0;
+  wire flag_destuff = 0;
+  reg [0:31] Clock_Count = 0;
+  reg [0:31] loop = 0;
  
   // Takes in input byte and serializes it 
   task CAN_WRITE_BYTE;
@@ -39,25 +43,83 @@ module can_tb ();
 	 //input		  i_IDE; //
 	 //input		  i_RESERVED0; //
 	 //input [3:0] i_length; //
-	 input [0:107] i_Data; //
+	 input [0:110] i_Data; //
 	 //input [14:0] i_CRC; //
 	 //input		  i_CRC_DELIM; //
 	 //input		  i_ACK; //
 	 //input		  i_ACK_DELIM; //
 	 //input		  i_STOP; //
-    integer     ii;
+    reg [0:7]     ii;
 	 //integer		  count_stuff_0 = 0;
 	 //integer		  count_stuff_1 = 0;
     begin
        
       // Send All Data
-		for (ii=0; ii<108; ii=ii+1)
+		for (ii=0; ii<111; ii=ii+1)
 			begin
 				r_Ds_Serial <= i_Data[ii];
 				#(c_BIT_PERIOD);
-				
+				if (flag_destuff == 1)
+					ii = ii + 1;
+					//r_Rx_Serial <= i_Data[ii];
 				r_Rx_Serial <= i_Data[ii];
+				//else
+					//r_Rx_Serial <= i_Data[ii+1];
 				#(c_BIT_PERIOD);
+				/*
+				$display("Indice antes(%d)	Bit: %b", ii, i_Data[ii]);
+				case (loop)
+					s_DESTUFF :
+						begin
+							//$display("Clock_count=%b	c_CLKS_PER_BIT=%b", Clock_Count, c_CLKS_PER_BIT);
+							if (Clock_Count == (c_CLKS_PER_BIT-1))
+								begin
+									//$display("Entrei loop");
+									Clock_Count = 0;
+									//if (flag_destuff == 0)
+										//r_Rx_Serial <= i_Data[ii];
+									//else
+									if(flag_destuff == 1)
+										begin
+											$display("Flaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaag=1");
+											ii = ii + 1;
+											//r_Rx_Serial <= i_Data[ii];
+										end
+									$display("Indice durante(%d)	Bit: %b", ii, i_Data[ii]);
+									//#((c_BIT_PERIOD-1)/2);
+									$display("s_RX = %b", s_RX);
+									$display("loop = %b", loop);
+									loop = s_RX;
+								end
+							else
+								begin
+									$display("Clock_count=%b	c_CLKS_PER_BIT=%b", Clock_Count, c_CLKS_PER_BIT);
+									Clock_Count = Clock_Count + 1;
+									loop = s_DESTUFF;
+								end
+						end
+						
+					s_RX :
+						begin
+							$display("Entrei RX");
+							if (Clock_Count < c_CLKS_PER_BIT-1)
+								begin
+									Clock_Count = Clock_Count + 1;
+									loop = s_RX;
+								end
+							else
+								begin
+									Clock_Count = 0;
+									r_Rx_Serial = i_Data[ii];
+									$display("Indice depois(%d)	Bit: %b", ii, i_Data[ii]);
+									//#(c_BIT_PERIOD);
+									loop = s_DESTUFF;
+								end
+						end
+						
+				   //default :
+						//loop = s_DESTUFF;
+				endcase*/
 			end
      end
   endtask // CAN_WRITE_BYTE
@@ -69,8 +131,11 @@ module can_tb ();
 		.i_Ds_Serial(r_Ds_Serial),
 		.i_cont_0(cont_0),
 		.i_cont_1(cont_1),
-		.i_bit_index(ii),
-		.o_bit_index(ii)
+		//.i_bit_index(ii),
+		//.o_bit_index(ii),
+		.o_flag_destuff(flag_destuff),
+		.o_cont_0(cont_0),
+		.o_cont_1(cont_1)
 	);
 	
 	can_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) CAN_RX_INST
@@ -112,11 +177,12 @@ module can_tb ();
       @(posedge r_Clock);
       //CAN_WRITE_BYTE(11'b00000010100, 4'b0001, 64'h1, 15'b0100001100000001);
 		//data frame standard = start(1bit)+identifier(11bits)+RTR(1bit)+IDE(1bit)+r0(1bit)+length(4bits)+data(64bits)+CRC(15bits)+CRCDelimiter(1bit)+ACK(1bit)+ACKDelimiter(1bit)+(1bit)stop(1bit)
-		CAN_WRITE_BYTE(108'b000001001010000010001101010101010101010101010101010101010101010101010101010101010101001000011000001001011111111); //data frame standard
+		CAN_WRITE_BYTE(111'b000001001010000010001101010101010101010101010101010101010101010101010101010101010101001000011000001001011111111); //data frame standard
       @(posedge r_Clock);
-             
+      
+		$display("Frame: %b", w_Rx_Byte);
       // Check that the correct command was received
-      if (w_Rx_Byte == 108'b000001001010000010001101010101010101010101010101010101010101010101010101010101010101001000011000001001011111111)
+      if (w_Rx_Byte == 108'b000000010100000000110101010101010101010101010101010101010101010101010101010101010100100001100000001011111111)
         $display("Test Passed - Correct Byte Received");
       else
         $display("Test Failed - Incorrect Byte Received");
