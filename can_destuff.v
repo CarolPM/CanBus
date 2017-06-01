3705211d85dbba8bc0ba4bdbbbffc4ba85f9cc73
Carol Medeiros <cpm@cin.ufpe.br>
2017-06-01 14:05:24 -0300

    Destuff-Externo-Posedge

--- can_destuff.v
+++ can_destuff.v
@@ -2,37 +2,77 @@ module can_destuff
 	(
 		input	i_Clock,
 		input i_Ds_Serial,
-		input i_cont_0,
-		input i_cont_1,
-		output o_Ds_Serial
+		input [0:2] i_cont_0,
+		input [0:2] i_cont_1,
+		output [0:2] o_cont_0,
+		output [0:2] o_cont_1,
+		output [0:1] o_flag_destuff
 	);
 	
-	//caso seja o quinto bit repetido chama o proprio metodo atual em rx se nao salva o valor normalmente em rx
-	/*
-	if (i_Ds_Serial == 0)
-			i_cont_0 = i_cont_0 + 1;
-		else
-			i_cont_1 = i_cont_1 + 1;
-		if (i_cont_0 < 6 && i_cont_1 < 6)
-			begin
-				o_Ds_Serial <= i_Ds_Serial;
-				#(c_BIT_PERIOD);
-			end
-		else
-			if (i_cont_0 == 6)
-				begin
-					i_cont_0 <= 1; //contador zera dps soma 1 do novo bit
-					r_Rx_Serial <= 1; //bit stuffing
-					r_Rx_Serial <= i_start; //sexto bit 0
-					#(c_BIT_PERIOD);
-				end
-			else
-				begin
-					i_cont_1 <= 1; //contador zera dps soma 1 do novo bit
-					r_Rx_Serial <= 0; //bit stuffing
-					r_Rx_Serial <= i_start; //sexto bit 1
-					#(c_BIT_PERIOD);
-				end
-	*/			
+	parameter CLKS_PER_BIT  			= 10;
+	
+	reg [0:63]    Clock_Count         = 0;
+	reg Ds_Serial = 1'b0;
+	reg [0:2] cont_0 = 3'b0;
+	reg [0:2] cont_1 = 3'b0;
+	reg [0:1] flag_destuff = 0;
+	
+	always @(posedge i_Clock)
+		begin
+			Ds_Serial <= i_Ds_Serial;
+			cont_0 <= i_cont_0;
+			cont_1 <= i_cont_1;
+		end
 
+	always @(posedge i_Clock)
+		begin
+			// O destuff só pode considerar que ouve bits iguais sequenciais caso um dos contadores seja 0 e o outro 5.
+			// Se esse cuidado não for tomado os contadores ficarão sendo somados individualmente
+			// até que o primeiro chegue a 5 sem necessariamente ser sequencialmente.
+			// Assim sendo o código abaixo fica fazendo um switch entre o cont_0 e o cont_1
+			// onde um só será somado se o outro estiver zerado.
+			// Caso não esteja zerado o código passa a somar o contador do outro bit e zera o contador anterior.
+			 
+			 /*if (Clock_Count < CLKS_PER_BIT-1)
+              begin
+                Clock_Count <= Clock_Count + 1'b1;
+              end
+			else
+			begin*/
+			//#(CLKS_PER_BIT);
+				 //Clock_Count <= 0;
+				if (Ds_Serial == 1'b0)
+					if(cont_1 == 3'b0)
+						cont_0 <= cont_0 + 3'b1;
+					else
+						begin
+							cont_1 <= 1'b0;
+							cont_0 <= cont_0 + 3'b1;
+						end
+				else
+					if (cont_0 == 3'b0)
+						cont_1 <= cont_1 + 1'b1;
+					else
+						begin
+							cont_0 <= 1'b0;
+							cont_1 <= cont_1 + 1'b1;
+						end
+				
+				if (cont_0 == 3'b101 || cont_1 == 3'b101)
+					begin
+						flag_destuff <= 2'b1;	// se for o quinto bit aciona a flag pra o can_rx ignorar o proximo bit lá
+														//bit_prox_index <= bit_prox_index + 1; // Se for o quinto bit o destuff pula uma posição do índice (pula o bit stuff)
+					end								// O can_rx deve salvar o bit atual para só então atualizar a posição atual
+														// do bit com o valor de bit_index.
+				else
+					flag_destuff <= 2'b0;
+			//$display("cont_0: %b	cont_1: %b", cont_0, cont_1);
+			//end
+			//$display("flag: %b", flag_destuff);
+		end
+	
+	assign o_cont_0 = cont_0;
+	assign o_cont_1 = cont_1;
+	assign o_flag_destuff = flag_destuff;
+	
 endmodule
--- can_tb.v
+++ can_tb.v
@@ -1,57 +1,96 @@
-//////////////////////////////////////////////////////////////////////
-// File Downloaded from http://www.nandland.com
-//////////////////////////////////////////////////////////////////////
- 
-// This testbench will exercise both the UART Tx and Rx.
-// It sends out byte 0xAB over the transmitter
-// It then exercises the receive by receiving byte 0x3F
-
-
 `timescale 1ns/10ps
+ 
 `include "can_tx.v"
 `include "can_rx.v"
+`include "can_destuff.v"
  
 module can_tb ();
  
-
-  parameter c_CLOCK_PERIOD_NS = 100;   
+  parameter c_CLOCK_PERIOD_NS = 100;
   parameter c_CLKS_PER_BIT    = 10;
   parameter c_BIT_PERIOD      = 1000;
+  parameter s_DESTUFF  		   = 0;
+  parameter s_RX		   		= 1;
+  
   reg r_Clock = 0;
+  reg r_Tx_DV = 0;
+  wire w_Tx_Done;
   reg r_Rx_Serial = 1;
-  wire [0:107] w_Rx_Byte; 
-   
+  reg r_Ds_Serial = 1;
+  wire [0:107] w_Rx_Byte; //start+identifier+RTR+IDE+RESERVED0+data
+  wire [0:2] w_cont_0;
+  wire [0:2] w_cont_1;
+  wire [0:1] w_flag_destuff;
+  reg [0:1] flag_destuff = 2'b0;
+  reg [0:2] cont_0 = 3'b0;
+  reg [0:2] cont_1 = 3'b0;
+
+  reg [0:31] Clock_Count = 0;
+  reg [0:31] loop = 0;
  
+  // Takes in input byte and serializes it 
   task CAN_WRITE_BYTE;
-	 input [0:107] i_Data;
-    integer     ii;
+	 input [0:107] i_Data; //
+    reg [0:7]     ii;
+
     begin
+       
+      // Send All Data
 		for (ii=0; ii<108; ii=ii+1)
 			begin
+				cont_0 = w_cont_0;
+				cont_1 = w_cont_1;
+				r_Ds_Serial <= i_Data[ii];
+				//#(c_BIT_PERIOD-1);
+				
+				if (w_flag_destuff == 2'b1)
+					ii = ii + 1;
 				r_Rx_Serial <= i_Data[ii];
-				#(c_BIT_PERIOD);
+
+				#(c_BIT_PERIOD-100);
+
 			end
      end
-  endtask
+  endtask // CAN_WRITE_BYTE
    
-   
-  can_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) CAN_RX_INST
-    (.i_Clock(r_Clock),
-     .i_Rx_Serial(r_Rx_Serial),
-     .o_Rx_DV(),
-     .o_Rx_Byte(w_Rx_Byte)
-     );
+
+	can_destuff #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) CAN_DESTUFF_INST
+	(
+		.i_Clock(r_Clock),
+		.i_Ds_Serial(r_Ds_Serial),
+		.i_cont_0(cont_0),
+		.i_cont_1(cont_1),
+		.o_flag_destuff(w_flag_destuff),
+		.o_cont_0(w_cont_0),
+		.o_cont_1(w_cont_1)
+	);
+	
+	can_rx #(.CLKS_PER_BIT(c_CLKS_PER_BIT)) CAN_RX_INST
+	(
+		.i_Clock(r_Clock),
+		.i_Rx_Serial(r_Rx_Serial),
+		.o_Rx_DV(),
+		.o_Rx_Byte(w_Rx_Byte)
+    );
 
    
   always
     #(c_CLOCK_PERIOD_NS/2) r_Clock <= !r_Clock;
  
    
-
+  // Main Testing:
   initial
     begin
+
+      // Send a command to the CAN (exercise Rx)
       @(posedge r_Clock);
-		  CAN_WRITE_BYTE(108'b011111111111000000100000000101010101010100111111111100001111111000000100000000101010101010100110000000111001); //data frame standard
+
+		//data frame normal
+		CAN_WRITE_BYTE(108'b011111011111010000010100000100010101010101010011111111110000111110110000010100000100010101010101010011000001); //data frame standard
+      @(posedge r_Clock);
+      
+		$display("Frame: %b", w_Rx_Byte);
+       
     end
    
 endmodule
